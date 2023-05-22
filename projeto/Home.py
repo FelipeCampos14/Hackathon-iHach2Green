e72859cd6cc77eef52f673394137fd2ab770f3e9
Felipe Campos <campos.felps@gmail.com>
2023-05-21 22:19:52 -0300

    Update Home.py

--- projeto/Home.py
+++ projeto/Home.py
@@ -8,7 +8,7 @@ st.image(logo)
 
 # Add logo to the sidebar
 logo_path = "projeto/img/logo2.png"
-st.sidebar.image(logo_path, width=350)
+st.sidebar.image(logo_path, width=300)
 
 # Description section
 st.header("Descrição")
