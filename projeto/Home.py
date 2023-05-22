3bf9f3a7b52094057b45b73916f36b34ea55078e
Felipe Campos <campos.felps@gmail.com>
2023-05-21 21:41:23 -0300

    Mais

--- projeto/Home.py
+++ projeto/Home.py
@@ -4,20 +4,20 @@ from PIL import Image
 st.title("Hydro Capital")
 # Logo
 image_path = "projeto/img/logo.png"
-st.image(image_path, caption="Image caption", width="300px")
+st.image(image_path, caption="Image caption", width=200)
 
-# # Description section
-# st.header("Description")
-# st.write("Bem-vindo à Hydro Capital! Somos uma equipe de profissionais dedicados ao avanço da energia sustentável, especialmente no campo do hidrogênio verde. Nossa missão é fornecer informações valiosas, análises e orientações financeiras para investimentos em projetos de hidrogênio verde. Seja você um investidor individual, uma empresa ou uma instituição financeira, a Hydro Capital é sua fonte de referência para compreender o potencial e a lucratividade das empreitadas de hidrogênio verde.")
+# Description section
+st.header("Description")
+st.write("Bem-vindo à Hydro Capital! Somos uma equipe de profissionais dedicados ao avanço da energia sustentável, especialmente no campo do hidrogênio verde. Nossa missão é fornecer informações valiosas, análises e orientações financeiras para investimentos em projetos de hidrogênio verde. Seja você um investidor individual, uma empresa ou uma instituição financeira, a Hydro Capital é sua fonte de referência para compreender o potencial e a lucratividade das empreitadas de hidrogênio verde.")
 
-# # Team section
-# st.header("Team")
+# Team section
+st.header("Team")
 
-# # Team member 1
-# st.subheader("Celine de Souza")
-# team_member1_image = Image.open('img/Celine.jpeg')
-# st.image(team_member1_image, width=200)
-# st.write("John Doe is a seasoned energy analyst with a deep understanding of the green hydrogen market. With his expertise in financial modeling and market research, John brings valuable insights to our team.")
+# Team member 1
+st.subheader("Celine de Souza")
+image_path = "projeto/img/Celine.png"
+st.image(image_path, caption="Image caption")
+st.write("John Doe is a seasoned energy analyst with a deep understanding of the green hydrogen market. With his expertise in financial modeling and market research, John brings valuable insights to our team.")
 
 # # Team member 2
 # st.subheader("Felipe Campos")
