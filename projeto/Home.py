5c577d6cba898b014eb9d29ff41655936f37dfeb
Felipe Campos <campos.felps@gmail.com>
2023-05-21 21:44:32 -0300

    VAi

--- projeto/Home.py
+++ projeto/Home.py
@@ -3,8 +3,8 @@ from PIL import Image
 
 st.title("Hydro Capital")
 # Logo
-image_path = "projeto/img/logo.png"
-st.image(image_path, caption="Image caption", width=200)
+logo = "projeto/img/logo.png"
+st.image(logo, width=200)
 
 # Description section
 st.header("Description")
@@ -15,27 +15,27 @@ st.header("Team")
 
 # Team member 1
 st.subheader("Celine de Souza")
-image_path = "projeto/img/Celine.png"
-st.image(image_path, caption="Image caption")
+Celine = "projeto/img/Celine.png"
+st.image(Celine, width=200)
 st.write("John Doe is a seasoned energy analyst with a deep understanding of the green hydrogen market. With his expertise in financial modeling and market research, John brings valuable insights to our team.")
 
-# # Team member 2
-# st.subheader("Felipe Campos")
-# team_member2_image = Image.open('img/Felipe.jpeg')
-# st.image(team_member2_image, width=200)
-# st.write("Jane Smith is an experienced renewable energy engineer with a passion for sustainable technologies. Her technical expertise and project management skills make her an invaluable asset to our team.")
+# Team member 2
+st.subheader("Felipe Campos")
+Felipe = "projeto/img/Felipe.png"
+st.image(Felipe, width=200)
+st.write("Jane Smith is an experienced renewable energy engineer with a passion for sustainable technologies. Her technical expertise and project management skills make her an invaluable asset to our team.")
 
-# # Team member 3
-# st.subheader("Vinicius ")
-# team_member3_image = Image.open('img/Vinicius.jpeg')
-# st.image(team_member3_image, width=200)
-# st.write("Michael Johnson is a financial analyst with a strong background in investment strategies. His knowledge of the green energy sector and financial markets adds a unique perspective to our team.")
+# Team member 3
+st.subheader("Vinicius ")
+Vinicius = "projeto/img/Vinicius.png"
+st.image(Vinicius, width=200)
+st.write("Michael Johnson is a financial analyst with a strong background in investment strategies. His knowledge of the green energy sector and financial markets adds a unique perspective to our team.")
 
-# # Team member 4
-# st.subheader("Emily Wilson")
-# team_member4_image = Image.open('img/Vivian.jpeg')
-# st.image(team_member4_image, width=200)
-# st.write("Emily Wilson is a communications specialist with expertise in content creation and marketing. Her skills in storytelling and branding help us effectively convey our message to a wide audience.")
+# Team member 4
+st.subheader("Emily Wilson")
+Vivian = "projeto/img/Vivian.png"
+st.image(Vivian, width=200)
+st.write("Emily Wilson is a communications specialist with expertise in content creation and marketing. Her skills in storytelling and branding help us effectively convey our message to a wide audience.")
 
 st.markdown(
     """
